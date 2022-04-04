9a115137fd601468d2df9fb54228e78b2a52062d
yann <yannatkk@gmail.com>
2022-04-04 16:26:17 +0200

    refactor home screen code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:project/models/user.dart';
-import 'package:project/screens/onboarding_screen.dart';
-import 'package:project/screens/product_screen.dart';
-
-import '../animations/page_transition.dart';
-import '../animations/slide_animation.dart';
-import '../utils/constants.dart';
+import 'package:project/screens/add_product_screen.dart';
+import 'package:project/screens/my_product_screen.dart';
+import 'package:project/screens/product_list_screen.dart';
+import 'package:project/screens/profile_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -19,160 +14,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final double _padding = 24;
+  int selectedIndex = 0;
 
-  late PageController _pageController;
-
-  User? user = FirebaseAuth.instance.currentUser;
-  AppUser loggedInUser = AppUser();
-
-  @override
-  void initState() {
-    _pageController = PageController(viewportFraction: 0.9);
-    super.initState();
-
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(user!.uid)
-        .get()
-        .then((value) {
-      loggedInUser = AppUser.fromMap(value.data());
-      setState(() {});
-    });
-  }
+  final Widget _productList = const ProductListScreen();
+  final Widget _addProduct = const AddProductScreen();
+  final Widget _myProduct = const MyProductScreen();
+  final Widget _profile = const ProfileScreen();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 35.h),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: _padding),
-              child: const _AppBar(),
-            ),
-            SizedBox(height: 24.h),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: _padding),
-              child: const _Header(),
-            ),
-            SizedBox(height: 24.h),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: _padding),
-              child: const SlideAnimation(child: _CategoryList()),
-            ),
-            SizedBox(height: 24.h),
-            SlideAnimation(
-              begin: Offset(400.w, 0),
-              child: SizedBox(
-                height: 500,
-                child: PageView.builder(
-                  controller: _pageController,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            PageTransition(
-                              child: const ProductScreen(),
-                              type: PageTransitionType.fadeIn,
-                            ));
-                      },
-                      child: Container(
-                        margin: EdgeInsets.only(right: 20.w),
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: Colors.black26),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 12,
-                                vertical: 12.h,
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    'BYDDR ',
-                                    style: TextStyle(
-                                      fontSize: 20.r,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(width: 12.w),
-                                  Text(
-                                    'JOUR 74',
-                                    style: TextStyle(
-                                      fontSize: 14.r,
-                                    ),
-                                  ),
-                                  const Spacer(),
-                                  Text(
-                                    '@${loggedInUser.firstName} ${loggedInUser.lastName}',
-                                    style: TextStyle(
-                                      fontSize: 14.r,
-                                      color: Colors.black54,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              child: Hero(
-                                tag: '$index',
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).primaryColor,
-                                    image: DecorationImage(
-                                      image: AssetImage(
-                                        index % 2 == 0
-                                            ? 'assets/images/image-0.jpg'
-                                            : 'assets/images/image-1.jpeg',
-                                      ),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 12.h),
-                            Padding(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 12,
-                                vertical: 12.h,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: const <Widget>[
-                                  EventStat(
-                                    title: '20h: 25m: 08s',
-                                    subtitle: 'Temps Restant',
-                                  ),
-                                  EventStat(
-                                    title: '15.97 BTC',
-                                    subtitle: 'Meilleure Offre',
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: getBody(),
       bottomNavigationBar: BottomNavigationBar(
         elevation: 0,
         iconSize: 22.r,
@@ -181,33 +33,66 @@ class _HomeScreenState extends State<HomeScreen> {
         showSelectedLabels: false,
         showUnselectedLabels: false,
         type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: BottomIcon(
-              icon: Iconsax.home_15,
-              isActive: true,
-            ),
-            label: 'Accueil',
-          ),
-          BottomNavigationBarItem(
-            icon: BottomIcon(icon: Iconsax.discover_1),
-            label: 'Explorer',
-          ),
-          BottomNavigationBarItem(
-            icon: BottomIcon(icon: Iconsax.add_square),
-            label: 'Vendre',
-          ),
-          BottomNavigationBarItem(
-            icon: BottomIcon(icon: Iconsax.shop),
-            label: 'Shop',
-          ),
-          BottomNavigationBarItem(
-            icon: BottomIcon(icon: Iconsax.wallet_3),
-            label: 'Compte',
-          ),
+        items: [
+          productList(),
+          addProduct(),
+          myProduct(),
+          profile(),
         ],
+        onTap: (int index) {
+          onTapHandler(index);
+        },
       ),
     );
+  }
+
+  BottomNavigationBarItem profile() {
+    return const BottomNavigationBarItem(
+      icon: BottomIcon(icon: Iconsax.profile_2user),
+      label: 'Compte',
+    );
+  }
+
+  BottomNavigationBarItem myProduct() {
+    return const BottomNavigationBarItem(
+      icon: BottomIcon(icon: Iconsax.wallet_3),
+      label: 'Mes produits',
+    );
+  }
+
+  BottomNavigationBarItem addProduct() {
+    return const BottomNavigationBarItem(
+      icon: BottomIcon(icon: Iconsax.add_square),
+      label: 'Vendre',
+    );
+  }
+
+  BottomNavigationBarItem productList() {
+    return const BottomNavigationBarItem(
+      icon: BottomIcon(
+        icon: Iconsax.home_15,
+        isActive: true,
+      ),
+      label: 'Accueil',
+    );
+  }
+
+  Widget getBody() {
+    if (selectedIndex == 0) {
+      return _productList;
+    } else if (selectedIndex == 1) {
+      return _addProduct;
+    } else if (selectedIndex == 2) {
+      return _myProduct;
+    } else {
+      return _profile;
+    }
+  }
+
+  void onTapHandler(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 }
 
@@ -228,114 +113,6 @@ class BottomIcon extends StatelessWidget {
           endIndent: 25.w,
           color: isActive ? Colors.black : Colors.transparent,
         )
-      ],
-    );
-  }
-}
-
-class _CategoryList extends StatelessWidget {
-  const _CategoryList({Key? key}) : super(key: key);
-
-  final List<String> _options = const [
-    'Tendance',
-    'Arts Digital',
-    'Vidéos 3D',
-    'Jeux',
-    'PDF',
-    'Collections',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 28.h,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: _options.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(4),
-              color: index == 0 ? Colors.black : null,
-            ),
-            padding: EdgeInsets.only(
-              left: 22.w,
-              right: index == 0 ? 22.w : 0,
-            ),
-            child: Center(
-              child: Text(
-                _options[index],
-                style: TextStyle(
-                  fontSize: 14.r,
-                  color: index == 0 ? Colors.white : Colors.black54,
-                  fontWeight: index == 0 ? FontWeight.bold : FontWeight.normal,
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class _Header extends StatelessWidget {
-  const _Header({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Live',
-              style: bodyTextStyle,
-            ),
-            SizedBox(height: 8.h),
-            Text(
-              'Enchères',
-              style: TextStyle(
-                fontSize: 26.r,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 2.h),
-            Text(
-              'Profitez! Les dernières enchères à la Une',
-              style: bodyTextStyle,
-            ),
-          ],
-        ),
-        const Icon(Iconsax.setting_4),
-      ],
-    );
-  }
-}
-
-class _AppBar extends StatelessWidget {
-  const _AppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const AppLogo(),
-        Container(
-          width: 40.r,
-          height: 40.r,
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              image: AssetImage('assets/images/profile.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
       ],
     );
   }
---
+++ lib/screens/my_product_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyProductScreen extends StatelessWidget {
+  const MyProductScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("My product screen"));
+  }
+}
